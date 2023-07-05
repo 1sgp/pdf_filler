8cf85069b9daa0da070dacb1fa3e27734f69b8d7
1sgp <138119976+1sgp@users.noreply.github.com>
2023-07-05 11:59:40 +0200

    Update 0.6.1 & 0.6.2
    - fullautomation via main() function
    - commented out prints

--- KlassenbuchAIO_a.py
+++ KlassenbuchAIO_a.py
@@ -1,10 +1,11 @@
 # Klassenbuchscraper AiO Package
-# version 0.6 ALPHA for 'pdf_filler' by mxwmnn
-# 07/04/2023
+# version 0.6.2 ALPHA for 'pdf_filler' by mxwmnn
+# 07/05/2023
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 from bs4 import re
 from time import sleep
@@ -12,44 +13,39 @@ import getpass
 import os
 import sys
 
-# add username and password for automation
-# useri = 'name@domain.com'
-# passi = 'Password'
-
 options=Options()
 options.add_argument('-headless')
 browser = webdriver.Firefox(options=options)
 
 def countdown(x):
     sleep(1)
-    print('')
+    #print('')
     sleep(1)
-    print(f'{x}', "seconds left before refreshing.", end="\r")
+    #print(f'{x}', "seconds left before refreshing.", end="\r")
     sleep(1)
     for count in range(x, 1, -1):   
-        print(f'{count}', "seconds left before refreshing.", end='\r')
+        #print(f'{count}', "seconds left before refreshing.", end='\r')
         sleep(1)
-    print("1 second left before refreshing.", end='\r')
+    #print("1 second left before refreshing.", end='\r')
     sleep(1)
     sleep(0.2)
-    print('')
-    print("Refreshing..")
+    #print('')
+    #print("Refreshing..")
     sleep(0.8)
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def enterCreds():
-    print("Please enter your credentials. You may correct them, if the login doesn't work.")
+    #print("Please enter your credentials. You may correct them, if the login doesn't work.")
     User = input('Username: ')
     Pass = getpass.getpass(prompt='Password: ')
     if User == "":
-        print('Please enter a username.')
+        #print('Please enter a username.')
         User = input('Username: ')
     if Pass == "":
         Pass = getpass.getpass(prompt='Please enter a password: ')
-    else:
-        print('Credentials accepted.')
+        #print('Credentials accepted.')
     sleep(1)
     clear()
     return User, Pass
@@ -58,20 +54,20 @@ def checkCreds(User, Pass):
     if User == "":
         User = input('Username: ')
     else:
-        print('Username: ', User)
+        #print('Username: ', User)
         changeit = input('Do you want to change the username? (N/Y):')
         if changeit.lower() == "y":
             User = input('Username: ')
             clear()
-            print(User)
+            #print(User)
     if Pass == "":
-        print('Please enter a valid password')
+        #print('Please enter a valid password')
         Pass = getpass.getpass(prompt='Enter a password please: ')
     else:
         pwc = input('Do you want to VISIBLY check your password? (N/Y):')       
     if pwc.lower() == "y":
-        print('Username: ', User)
-        print('Password: ', Pass)
+        #print('Username: ', User)
+        #print('Password: ', Pass)
         sleep(3)
         clear()      
     pwr = input('Do you want to change your password? (N/Y):') 
@@ -85,7 +81,7 @@ def loginUser(User, Pass):
         username = browser.find_element(By.ID, 'username')
         password = browser.find_element(By.ID, 'password')
         loginbut = browser.find_element(By.CLASS_NAME, 'btn-primary')
-        print("Logging in ..")
+        #print("Logging in ..")
         username.clear()
         username.send_keys(User)
         password.clear()
@@ -93,21 +89,21 @@ def loginUser(User, Pass):
         loginbut.click()
         sleep(1)
         if "Ungültige Anmeldedaten. Versuchen Sie es noch einmal!" in browser.page_source:
-            print("Wrong credentials. Please check your input!")
+            #print("Wrong credentials. Please check your input!")
             User, Pass = checkCreds(User, Pass)
             sleep(1.5)
             loginUser(User, Pass)
         else:
             logged_in = True
-            print('Login successful!')
+            #print('Login successful!')
             return
     except NoSuchElementException:
-        print('Already signed in.')
+        #print('Already signed in.')
         logged_in = True
-        return logged_in
+        return
 
 def Kursmenu():
-    global Kurse
+#    global Kurse
     Kurse = {}    
     browser.get('https://lernplattform.gfn.de/my/')
     soup = BeautifulSoup(browser.page_source, 'html.parser')
@@ -124,10 +120,17 @@ def Kursmenu():
 def klassenbucher(Kurse):
     classbooks = {}
     for key in Kurse.keys():
-        linkstr = Kurse[key] + '&section=5'
-        sleep(0.1)
+        browser.get(Kurse[key])
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        Klassenbuch = soup.find('h3', string = 'Klassenbuch')
+        section = '&section=' + Klassenbuch.parent.get('id')[-1]
+        linkstr = Kurse[key] + f'{section}'
+        sleep(0.5)
         browser.get(linkstr)
-        klassenbuchelement = browser.find_element(By.CLASS_NAME, 'activitytitle')
+        try:
+            klassenbuchelement = browser.find_element(By.CLASS_NAME, 'activitytitle')
+        except NoSuchElementException:
+            continue
         klassenbuchelement.click()
         sleep(0.5)
         anzeigelink = browser.current_url + '&view=5'
@@ -148,14 +151,19 @@ def klassenbucher(Kurse):
             sleep(1)
     return classbooks
 
-useri, passi = enterCreds()
-loginUser(useri, passi)
-sleep(1)
-clear()
-Kurse = Kursmenu()
-output = klassenbucher(Kurse)
-
-while True:
+def main(benutzer, passwort):
+    useri, passi = benutzer, passwort if benutzer != "" else enterCreds()
+    loginUser(useri, passi)
+    sleep(1)
     clear()
-    print(output)
-    countdown(10)
+    Kurse = Kursmenu()
+    output = klassenbucher(Kurse)
+#     while True:
+#         clear()
+#         print(output)
+#         countdown(10)    
+    return output
+    
+if __name__ == '__main__':
+    benutzer, passwort = enterCreds()
+    main(benutzer, passwort)
