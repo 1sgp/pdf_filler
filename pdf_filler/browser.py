87c12e9e5a2fe9e55f92dbcb378ecc12b0f47b9b
1sgp <138119976+1sgp@users.noreply.github.com>
2023-09-01 12:27:55 +0200

    browser.py
    left overs removed

--- pdf_filler/browser.py
+++ pdf_filler/browser.py
@@ -1,17 +1,11 @@
 # browser
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
-from bs4 import BeautifulSoup as bs
-from time import sleep
 
 def init():
     options=Options()
-    #options.add_argument('-headless')
+    options.add_argument('-headless')
     browser=webdriver.Firefox(options=options)
     return browser
 
